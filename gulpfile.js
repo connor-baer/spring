8db27b6673d56dfe904617ace8973f0cbaf3cd17
Connor Bär <connor.baer@sumup.com>
2019-01-19 02:08:18 +0100

    Install @sumup/foundry for linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,38 @@
 // Include plugins
-var gulp = require( 'gulp' ),
-  	shell = require( 'gulp-shell' ),
-  	browserSync = require( 'browser-sync' ).create(),
-  	jshint = require( 'gulp-jshint' ),
-    plumber = require( 'gulp-plumber' ),
-  	rename = require( 'gulp-rename' ),
-  	uglify = require( 'gulp-uglify' );
+const gulp = require('gulp');
+
+const shell = require('gulp-shell');
+
+const browserSync = require('browser-sync').create();
+
+const plumber = require('gulp-plumber');
+
+const rename = require('gulp-rename');
+
+const uglify = require('gulp-uglify');
 
 // Task for building blog when something changed:
-gulp.task( 'build', shell.task(['jekyll build --watch']) );
+gulp.task('build', shell.task(['jekyll build --watch']));
 
 // Task for serving blog with Browsersync
-gulp.task( 'serve', function() {
-    browserSync.init({ server: { baseDir: '_site/' }, notify: false, reloadDelay: 2000, injectChanges: false });
-    // Reloads page when some of the already built files changed:
-    gulp.watch( '_site/**/*.*' ).on( 'change', browserSync.reload );
+gulp.task('serve', () => {
+  browserSync.init({
+    server: { baseDir: '_site/' },
+    notify: false,
+    reloadDelay: 2000,
+    injectChanges: false
+  });
+  // Reloads page when some of the already built files changed:
+  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task( 'default', ['scripts', 'build', 'serve']);
+gulp.task('default', ['scripts', 'build', 'serve']);
 
-gulp.task( 'scripts', function() {
-  return gulp.src( 'assets/scripts.js' )
-  .pipe( plumber() )
-  .pipe( uglify() )
-  .pipe( rename( 'scripts.min.js'))
-  .pipe(gulp.dest('assets'));
-});
+gulp.task('scripts', () =>
+  gulp
+    .src('assets/scripts.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('assets'))
+);
